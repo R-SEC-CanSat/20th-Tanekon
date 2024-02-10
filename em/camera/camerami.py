6bd44ae23824910d1e4771329969e124d05eac36
Riho0 <misaki04141910035@gmail.com>
2024-02-10 15:46:46 +0900

    カメラ修正

--- em/camera/camerami.py
+++ em/camera/camerami.py
@@ -1,58 +1,54 @@
-from fpioa_manager import fm
-from machine import UART
-import time
 import sensor
 import image
+from fpioa_manager import fm
+from machine import UART
+from Maix import GPIO
+from modules import ws2812
+import time
 
-fm.register(10, fm.fpioa.UART1_TX, force=True)
-fm.register(11, fm.fpioa.UART1_RX, force=True)
-uart = UART(UART.UART1, 115200, 8, 1, 0, timeout=1000, read_buf_len=4096)
+# RGB LED設定
+class_ws2812 = ws2812(8,100)
+BRIGHTNESS = 0x10
+
+fm.register(35, fm.fpioa.UART2_TX, force=True)
+fm.register(34, fm.fpioa.UART2_RX, force=True)
+uart = UART(UART.UART2, 57600, 8, None, 1, timeout=1000, read_buf_len= 4096)
+
+def RGB_LED(r,g,b):
+    a = class_ws2812.set_led(0,(r,g,b))
+    a = class_ws2812.display()
+    time.sleep(0.3)
+    a = class_ws2812.set_led(0,(0,0,0))
+    a = class_ws2812.display()
+    time.sleep(0.3)
+    a = class_ws2812.set_led(0,(r,g,b))
+    a = class_ws2812.display()
+
+# 起動インジケータとしてblueで点滅、最後に点灯
+RGB_LED(0,0,BRIGHTNESS)
+RGB_LED(0,0,BRIGHTNESS)
+RGB_LED(0,0,BRIGHTNESS)
+
 sensor.reset()                      # カメラの初期化
 sensor.set_pixformat(sensor.RGB565) # RGB565フォーマットを使用
 sensor.set_framesize(sensor.QVGA)   # QVGAフレームサイズを使用
 sensor.set_auto_gain(False)         # オートゲインをオフ
-sensor.__write_reg(0x80, 0xEF)      # オートホワイトバランスをオフ、白飛び防止
+sensor.__write_reg(0x80, 0xEF)      # オートホワイトバランスをオフ
 sensor.skip_frames(time = 2000)     # 2秒間フレームをスキップして安定化
+
 threshold = [(30, 100,  15, 127,  15, 127)] # 検出する色のしきい値を設定
-sensor.run(1)# 連続取得開始
 
-Px = -0.4
-
-while True:
-    img = sensor.snapshot()
-    #連続ピクセルの塊の位置と大きさを取得
-    blobs = img.find_blobs(threshold, x_stride = 2, y_stride = 2, pixels_threshold = 100, merge = True, margin = 20)
+while(True):
+    img = sensor.snapshot()           # 画像を取得
+    blobs = img.find_blobs(threshold) # しきい値内の色を検出
     if blobs:
-        max_area = 0
-        target = blobs[0]
-        for b in blobs:
-            if b.area() > max_area:
-                max_area = b.area()
-                target = b
+        max_blob = max(blobs, key=lambda b: b.area()) # 面積が最大の領域を取得
+        max_per = max_blob.area()/76800
+        img.draw_rectangle(max_blob[0:4])             # 検出した色を矩形で囲む
+        img.draw_cross(max_blob[5], max_blob[6])# 検出した色の中心に十字を描く
+        sendstr = str(max_blob[5])+","+str(max_blob[6])+","+str(max_per)
+        for char in sendstr:
+            uart.write(char)
+        uart.write("\n")
+        print(max_blob[5], max_blob[6],max_per)
 
-        xerr = 160 - target[5]
-        ux = int(Px * xerr)
-
-        if ux > 100:
-            ux = 100
-        elif ux < -100:
-            ux = -100
-
-        if abs(xerr) < 20:
-            ux = 0
-        #print(ux)
-
-        if uart.read(4096):
-            area = target.area()
-            hexlist = [ux & 0xFF, (area >> 16) & 0xFF, (area >> 8) & 0xFF, area & 0xFF]
-            a = uart.write(bytes(hexlist))
-
-        #print(abs(prev_area - target.area()))
-        prev_area = target.area()
-        tmp=img.draw_rectangle(target[0:4])
-        tmp=img.draw_cross(target[5], target[6])
-        # c=img.get_pixel(target[5], target[6])
-    else:
-        if uart.read(1):
-            hexlist = [0x00, 0x00, 0x00, 0x00]
-            a = uart.write(bytes(hexlist))
