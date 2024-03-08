9b794ce0a590bff725a9503f3dfdaa92ad1b5aa4
Riho0 <misaki04141910035@gmail.com>
2024-03-08 13:15:56 +0900

    0305

--- etoe/0307etoe.cpp
+++ etoe/0307etoe.cpp
@@ -298,6 +298,30 @@ void stop(){
     digitalWrite(BIN2, LOW);
 }
 
+double distanceBetween(double lat1, double long1, double lat2, double long2){
+    // returns distance in meters between two positions, both specified
+    // as signed decimal-degrees latitude and longitude. Uses great-circle
+    // distance computation for hypothetical sphere of radius 6372795 meters.
+    // Because Earth is no exact sphere, rounding errors may be up to 0.5%.
+    // Courtesy of Maarten Lamers
+    double delta = radians(long1-long2);
+    double sdlong = sin(delta);
+    double cdlong = cos(delta);
+    lat1 = radians(lat1);
+    lat2 = radians(lat2);
+    double slat1 = sin(lat1);
+    double clat1 = cos(lat1);
+    double slat2 = sin(lat2);
+    double clat2 = cos(lat2);
+    delta = (clat1 * slat2) - (slat1 * clat2 * cdlong);
+    delta = sq(delta);
+    delta += sq(clat2 * sdlong);
+    delta = sqrt(delta);
+    double denom = (slat1 * slat2) + (clat1 * clat2 * cdlong);
+    delta = atan2(delta, denom);
+    return delta * 6372795;
+}
+
 //Read 80 bytes from I2C
 void readI2C(char *inBuff){
     Wire.requestFrom((uint8_t)DEFAULT_DEVICE_ADDRESS, (uint8_t) 80);
@@ -738,29 +762,6 @@ void setting(){
     servo2.write(90);
     servo3.write(75);
 }
-double distanceBetween(double lat1, double long1, double lat2, double long2){
-    // returns distance in meters between two positions, both specified
-    // as signed decimal-degrees latitude and longitude. Uses great-circle
-    // distance computation for hypothetical sphere of radius 6372795 meters.
-    // Because Earth is no exact sphere, rounding errors may be up to 0.5%.
-    // Courtesy of Maarten Lamers
-    double delta = radians(long1-long2);
-    double sdlong = sin(delta);
-    double cdlong = cos(delta);
-    lat1 = radians(lat1);
-    lat2 = radians(lat2);
-    double slat1 = sin(lat1);
-    double clat1 = cos(lat1);
-    double slat2 = sin(lat2);
-    double clat2 = cos(lat2);
-    delta = (clat1 * slat2) - (slat1 * clat2 * cdlong);
-    delta = sq(delta);
-    delta += sq(clat2 * sdlong);
-    delta = sqrt(delta);
-    double denom = (slat1 * slat2) + (clat1 * clat2 * cdlong);
-    delta = atan2(delta, denom);
-    return delta * 6372795;
-}
 
 void GetAzimuthDistance(double goallat, double goallon){
     GPS_data_run(goallat, goallon);
@@ -1101,6 +1102,7 @@ void SD_main_write(){
     
 }
 void loop() {
+    P_camera_Moter(1);
     if(progress == 'A'){
         SD_main_write();
         //release sequence_1
